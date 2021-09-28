6f00f1449e91c19ae1dc1adc15ac7a424b0a9bc6
jeo19 <terrakkk@outlook.com>
2021-09-28 19:19:45 +0800

    feat:Apply the Provider on store

--- src/index.js
+++ src/index.js
@@ -3,9 +3,14 @@ import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
 import { createStore } from "redux";
+import { Provider } from "react-redux";
 import rootReducer from "./modules";
 
 const store = createStore(rootReducer);
-console.log(store.getState());
 
-ReactDOM.render(<App />, document.getElementById("root"));
+ReactDOM.render(
+  <Provider store={store}>
+    <App />
+  </Provider>,
+  document.getElementById("root")
+);
