982988c1c1693c91577017ea680297d9954f9450
jeo19 <terrakkk@outlook.com>
2021-09-28 10:36:48 +0800

    feat:Create the root store with a createStore in index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,10 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.css";
 import App from "./App";
+import { createStore } from "redux";
+import rootReducer from "./modules";
+
+const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(<App />, document.getElementById("root"));
