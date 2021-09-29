39139a30b87fee7e59e2d4ddc640ef97d07bb8bf
jeo19 <terrakkk@outlook.com>
2021-09-29 11:16:51 +0800

    feat:Implement the CounterContainer component as a container component

--- src/index.js
+++ src/index.js
@@ -7,6 +7,7 @@ import { Provider } from "react-redux";
 import rootReducer from "./modules";
 
 const store = createStore(rootReducer);
+console.log(store.getState());
 
 ReactDOM.render(
   <Provider store={store}>
