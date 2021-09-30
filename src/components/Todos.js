b3e88ba62872864f4a43ff59848ecfa11da93cfa
jeo19 <terrakkk@outlook.com>
2021-09-30 19:49:28 +0800

    feat:Add the presentation component as todos component step01

---
+++ src/components/Todos.js
@@ -0,0 +1,25 @@
+import React, { useState } from "react";
+
+const TodoItem = React.memo(function TodoItem({ todo, onToggle }) {
+  return (
+    <li
+      style={{ textDecoration: todo.done ? "line-through" : "none" }}
+      onClick={() => onToggle(todo.id)}
+    >
+      {todo.text}
+    </li>
+  );
+});
+
+const TodoList = React.memo(function TodoList({ todos, onToggle }) {
+  return (
+    <ul>
+      {todos.map((todo) => (
+        <TodoItem key={todo.id} todo={todo} onToggle={onToggle} />
+      ))}
+    </ul>
+  );
+});
+function Todos({ todos, onCreate, onToggle }) {
+  const [text, setText] = useState("");
+}
